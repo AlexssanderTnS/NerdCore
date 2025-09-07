20037365437803de59461b3101640b01cd7a8fe9
AlexssanderTnS <alexssander.amat@gmail.com>
2025-09-07 18:11:55 -0300

    Estruturação das Pastas

--- SiteLoja/js/home.js
+++ SiteLoja/js/home.js
@@ -23,3 +23,12 @@ document.querySelectorAll('.camisa').forEach(img => {
   });
 });
 
+
+function clickMenu() {
+            const menuList = document.getElementById("menu-list");
+            if (menuList.style.display === "flex") {
+                menuList.style.display = "none";
+            } else {
+                menuList.style.display = "flex";
+            }
+        }
