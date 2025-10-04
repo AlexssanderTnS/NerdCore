c2bd85f426ebd5d91b81e0d8b5a9a156d727ffb1
AlexssanderTnS <alexssander.amat@gmail.com>
2025-10-03 23:40:57 -0300

    Ajuste do header e da barra de pesqsuisa, centralizando e criação do footer no login

---
+++ SiteLoja/php/footer.php
@@ -0,0 +1,14 @@
+<?php
+
+// Footer do site
+<footer>
+    <div>
+        <a href="https://www.facebook.com/NerdCore" target="_blank">Facebook</a> |
+        <a href="https://www.twitter.com/NerdCore" target="_blank">Twitter</a> |
+        <a href="https://www.instagram.com/NerdCore" target="_blank">Instagram</a>
+    </div>
+    <p>&copy; 2024 NerdCore. Todos os direitos reservados.</p>
+</footer>
+
+
+?>
