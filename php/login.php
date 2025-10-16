745e887e815c6b053d346eb2a91a92a078d96413
Ellen gama <ellencaroline485@hotmail.com>
2025-10-16 13:21:14 -0300

    remoção/php

--- php/login.php
+++
@@ -1,6 +0,0 @@
-<?php
-$nome = $_POST['nome'] ?? 'Erro: Campo nome não enviado';
-$usuario = $_POST['usuario'] ?? 'Erro: Campo usuário não enviado';
-echo "Nome: $nome <br> Usuário: $usuario";
-
-?>
