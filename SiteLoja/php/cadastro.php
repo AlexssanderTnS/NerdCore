f29533916613bbf79acff8245fadc4280624018a
Ellen gama <ellencaroline485@hotmail.com>
2025-10-16 16:38:19 -0300

    strings/form

---
+++ SiteLoja/php/cadastro.php
@@ -0,0 +1,64 @@
+<?php> 
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    //Elementos do formulário
+    $nome = $_POST['nome'];
+    $usuario = $_POST['usuario'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+    $confsenha =$_POST['confsenha'];
+    $date = $_POST['date'];
+    $mae = $POST['mae'];
+    $genero = $_POST['genero'];
+    $cpf = $_POST['cpf'];
+    $cll = $_POST['cll'];
+    $fixo = $_POST['fixo'];
+    $cep = $_POST['cep'];
+    $estado = $_POST['estado'];
+    $cidade = $_$POST['cidade'];
+    $bairro = $_POST['bairro'];
+    $numero = $_POST['numero'];
+
+    //Botões
+    
+
+
+
+
+
+
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+}
+?>
