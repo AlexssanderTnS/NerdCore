250eb4388594de327750ac092d1181cb696e8365
L Riolo <eduardo.william188@gmail.com>
2025-10-17 22:00:43 -0300

    Removendo BD do cadastro.php

--- SiteLoja/php/cadastro.php
+++ SiteLoja/php/cadastro.php
@@ -2,20 +2,7 @@
   if ($_SERVER['REQUEST_METHOD'] === "POST") {
 
     //conexão com o banco de dados
-    $host = "localhost";
-
-    $user= "root";
-
-    $pass = "";
-
-    $db = "nerdcore";
-
-
-    $conn = new mysqli ($host, $user, $pass, $db);
-      if ($conn ->connect_error) {
-            die ("Conexão falhou:" . $conn->connect_error);     
-
-    }
+    
 
     //Elementos do formulário
     $nome = $_POST['nome'] ?? '';
@@ -48,6 +35,8 @@
     }
     
     if ($senha !== $confsenha) {
+        
+    }else{
         die("As senhas não coincidem. Por favor, tente novamente.");
     }
      
@@ -59,7 +48,7 @@
         die("Nome de usuário inválido. Por favor, tente novamente.");
     }
 
-    if (!preg_match("/^(?!^(\d)\1{10}$)\d{11}$/", $cpf)){
+    if (!preg_match("/^(?!^(\d)\1{14}$)\d{14}$/", $cpf)){
         die("CPF iválido . Por favor, tente novamente.");
     }
 
@@ -71,55 +60,9 @@
    //Criptograia da senha
    $senhacriptografada = password_hash($senha, PASSWORD_DEFAULT);
 
-    //Conexão com o banco de dados   
+    
    echo "<p>Usuário cadastrado com sucesso!</p>";
 
-    $conn = new mysqli("localhost" , "root", "", "nerdcore");
-
-    if ($conn->connect_error){
-        die("Conexão falhou:". $conn->connect_error);
-    }
-
-    //Inserindo os dados no banco
-    $sql = "INSERT INTO usuarios
-         ( nome, usuario, email , senha, data_nascimento, mae , genero , cpf , cll , fixo , cep , estado , cidade , bairro , numero , complemento)
-        VALUES ( '$nome', '$usuario' , '$email' , '$senhacriptografada' , '$data_nascimento' , '$mae' , '$genero' , '$cpf' , '$cll' , '$fixo' , '$cep' , '$estado' ,'$cidade' , '$bairro' , '$numero' , '$complemento')";
-
-    $stmt= $conn->prepare($sql);
-    if (!$stmt){
-        die("Erro na preparação da declaração:" . $conn->error);
-    }
-
-    $stmt->bind_param(
-     "ssssssssssssssss",
-     $nome,
-     $usuario,
-     $email,
-     $senhacriptografada,
-     $data_nascimento,
-     $mae,
-     $genero,
-     $cpf,
-     $cll,
-     $fixo,
-     $cep,
-     $estado,
-     $cidade,
-     $bairro,
-     $numero,
-     $complemento
-    
-    );
-
-    if ($stmt->execute()){
-        echo "<p>Usuário cadastrado com sucesso!</p>";
-    } else {
-        echo "Erro ao cadastrar usuário: " . $stmt->error;
-    }
-
-
-     $stmt->close();
-     $conn->close();
-    
-}
+   
+  }
 ?>
